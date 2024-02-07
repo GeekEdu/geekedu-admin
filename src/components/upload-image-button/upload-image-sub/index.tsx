2c784d38c49e73f1c938471d927b7bf48930a3b1
Poison02 <2069820192@qq.com>
2024-02-07 15:40:24 +0800

    和后端联调实现图片库功能

--- src/components/upload-image-button/upload-image-sub/index.tsx
+++ src/components/upload-image-button/upload-image-sub/index.tsx
@@ -1,77 +1,80 @@
-import { Button, message, Modal } from "antd";
-import Dragger from "antd/es/upload/Dragger";
-import { useState } from "react";
-import config from "../../../js/config";
-import { getToken } from "../../../utils";
-import { InboxOutlined } from "@ant-design/icons";
+import { Button, Modal, message } from 'antd'
+import Dragger from 'antd/es/upload/Dragger'
+import { useState } from 'react'
+import { InboxOutlined } from '@ant-design/icons'
+import config from '../../../js/config'
+import { getToken } from '../../../utils'
 
 interface PropsInterface {
-  from: number;
-  onUpdate: () => void;
+  from: number
+  onUpdate: () => void
 }
 
-export const UploadImageSub = (props: PropsInterface) => {
-  const [showModal, setShowModal] = useState(false);
+export function UploadImageSub(props: PropsInterface) {
+  const [showModal, setShowModal] = useState(false)
 
   const uploadProps = {
-    name: "file",
+    name: 'file',
     multiple: true,
-    action: config.url + "/backend/api/v1/media/image?from=" + props.from,
+    // action: `${config.url}/backend/api/v1/media/image?from=${props.from}`,
+    action: `${config.url}/res/api/file/upload?from=${props.from}`,
     headers: {
-      authorization: "Bearer " + getToken(),
+      authorization: `Bearer ${getToken()}`,
     },
     onChange(info: any) {
-      const { status, response } = info.file;
-      if (status === "done") {
-        if (response.status === 0) {
-          message.success(`${info.file.name} 上传成功`);
-        }
-      } else if (status === "error") {
-        message.error(`${info.file.name} 上传失败`);
+      const { status, response } = info.file
+      if (status === 'done') {
+        if (response.status === 0)
+          message.success(`${info.file.name} 上传成功`)
+      }
+      else if (status === 'error') {
+        message.error(`${info.file.name} 上传失败`)
       }
     },
     showUploadList: {
       showRemoveIcon: false,
       showDownloadIcon: false,
     },
-  };
+  }
 
   return (
     <>
       <Button
         type="primary"
         onClick={() => {
-          setShowModal(true);
+          setShowModal(true)
         }}
       >
         上传图片
       </Button>
 
-      {showModal ? (
-        <Modal
-          open={true}
-          closable={false}
-          onCancel={() => {
-            setShowModal(false);
-          }}
-          onOk={() => {
-            props.onUpdate();
-            setShowModal(false);
-          }}
-          maskClosable={false}
-          centered
-        >
-          <Dragger {...uploadProps}>
-            <p className="ant-upload-drag-icon">
-              <InboxOutlined />
-            </p>
-            <p className="ant-upload-text">请将图片拖拽到此处上传</p>
-            <p className="ant-upload-hint">
-              支持一次上传多个 / 支持 png,jpg,jpeg,gif 格式图片
-            </p>
-          </Dragger>
-        </Modal>
-      ) : null}
+      {showModal
+        ? (
+          <Modal
+            open={true}
+            closable={false}
+            onCancel={() => {
+              setShowModal(false)
+            }}
+            onOk={() => {
+              props.onUpdate()
+              setShowModal(false)
+            }}
+            maskClosable={false}
+            centered
+          >
+            <Dragger {...uploadProps}>
+              <p className="ant-upload-drag-icon">
+                <InboxOutlined />
+              </p>
+              <p className="ant-upload-text">请将图片拖拽到此处上传</p>
+              <p className="ant-upload-hint">
+                支持一次上传多个 / 支持 png,jpg,jpeg,gif 格式图片
+              </p>
+            </Dragger>
+          </Modal>
+          )
+        : null}
     </>
-  );
-};
+  )
+}
--- src/pages/resource/images/index.tsx
+++ src/pages/resource/images/index.tsx
@@ -1,169 +1,172 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 import {
-  Spin,
   Button,
-  Row,
   Col,
-  Modal,
-  Image,
   Empty,
-  message,
+  Image,
+  Modal,
   Pagination,
-} from "antd";
-import { media } from "../../../api/index";
-import styles from "./index.module.scss";
-import { useDispatch } from "react-redux";
-import { titleAction } from "../../../store/user/loginUserSlice";
-import { UploadImageSub } from "../../../components/upload-image-button/upload-image-sub";
-import { ExclamationCircleFilled, CheckOutlined } from "@ant-design/icons";
-import { PerButton } from "../../../components";
+  Row,
+  Spin,
+  message,
+} from 'antd'
+import { useDispatch } from 'react-redux'
+import { CheckOutlined, ExclamationCircleFilled } from '@ant-design/icons'
+import { UploadImageSub } from '../../../components/upload-image-button/upload-image-sub'
+import { titleAction } from '../../../store/user/loginUserSlice'
+import { media } from '../../../api/index'
+import { PerButton } from '../../../components'
+import styles from './index.module.scss'
 
-const { confirm } = Modal;
+const { confirm } = Modal
 
 interface ImageItem {
-  id: number;
-  admin_id: number;
-  created_at: string;
-  disk: string;
-  extension: string;
-  name: string;
-  parent_id: number;
-  path: string;
-  size: number;
-  type: string;
+  id: number
+  admin_id: number
+  created_at: string
+  disk: string
+  extension: string
+  name: string
+  parent_id: number
+  path: string
+  size: number
+  type: string
 }
 
-const ResourceImagesPage = () => {
-  const dispatch = useDispatch();
-  const [imageList, setImageList] = useState<ImageItem[]>([]);
-  const [refresh, setRefresh] = useState(false);
-  const [page, setPage] = useState(1);
-  const [size, setSize] = useState(32);
-  const [total, setTotal] = useState(0);
-  const [selectKey, setSelectKey] = useState<number[]>([]);
-  const [visibleArr, setVisibleArr] = useState<boolean[]>([]);
-  const [hoverArr, setHoverArr] = useState<boolean[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [delLoading, setDelLoading] = useState(false);
+function ResourceImagesPage() {
+  const dispatch = useDispatch()
+  const [imageList, setImageList] = useState<ImageItem[]>([])
+  const [refresh, setRefresh] = useState(false)
+  const [pageNum, setPageNum] = useState(1)
+  const [pageSize, setPageSize] = useState(32)
+  const [total, setTotal] = useState(0)
+  const [selectKey, setSelectKey] = useState<number[]>([])
+  const [visibleArr, setVisibleArr] = useState<boolean[]>([])
+  const [hoverArr, setHoverArr] = useState<boolean[]>([])
+  const [loading, setLoading] = useState(false)
+  const [delLoading, setDelLoading] = useState(false)
 
+  // 初始化页面标题
   useEffect(() => {
-    document.title = "图片库";
-    dispatch(titleAction("图片库"));
-  }, []);
+    document.title = '图片库'
+    dispatch(titleAction('图片库'))
+  }, [])
 
   // 加载图片列表
   useEffect(() => {
-    getImageList();
-  }, [refresh, page, size]);
+    getImageList()
+  }, [refresh, pageNum, pageSize])
 
   // 删除图片
   const removeResource = () => {
-    if (selectKey.length === 0) {
-      return;
-    }
+    if (selectKey.length === 0)
+      return
+
     confirm({
-      title: "操作确认",
+      title: '操作确认',
       icon: <ExclamationCircleFilled />,
-      content: "确认删除选中图片？",
+      content: '确认删除选中图片？',
       centered: true,
-      okText: "确认",
-      cancelText: "取消",
+      okText: '确认',
+      cancelText: '取消',
       okButtonProps: { loading: delLoading },
       onOk() {
-        if (delLoading) {
-          return;
-        }
-        setDelLoading(true);
+        if (delLoading)
+          return
+
+        setDelLoading(true)
         media
           .destroyImages({
             ids: selectKey,
           })
           .then(() => {
-            setDelLoading(false);
-            message.success("删除成功");
-            resetImageList();
+            setDelLoading(false)
+            message.success('删除成功')
+            resetImageList()
           })
           .catch((e) => {
-            setDelLoading(false);
-          });
+            setDelLoading(false)
+          })
       },
       onCancel() {
-        console.log("Cancel");
+        console.log('Cancel')
       },
-    });
-  };
+    })
+  }
 
   // 获取图片列表
   const getImageList = () => {
-    setLoading(true);
+    setLoading(true)
+    // 请求获取图片列表
     media
       .imagesList({
-        page: page,
-        size: size,
+        pageNum,
+        pageSize,
       })
       .then((res: any) => {
-        setTotal(res.data.data.total);
-        setImageList(res.data.data.data);
-        let data: ImageItem[] = res.data.data.data;
-        let arr = [];
-        for (let i = 0; i < data.length; i++) {
-          arr.push(false);
-        }
-        setVisibleArr(arr);
-        setHoverArr(arr);
-        setLoading(false);
+        setTotal(res.data.data.total)
+        setImageList(res.data.data.data)
+        const data: ImageItem[] = res.data.data.data
+        const arr = []
+        for (let i = 0; i < data.length; i++)
+          arr.push(false)
+
+        setVisibleArr(arr)
+        setHoverArr(arr)
+        setLoading(false)
       })
       .catch((err: any) => {
-        setLoading(false);
-        console.log("错误,", err);
-      });
-  };
+        setLoading(false)
+        console.log('错误,', err)
+      })
+  }
   // 重置列表
   const resetImageList = () => {
-    setPage(1);
-    setImageList([]);
-    setSelectKey([]);
-    setRefresh(!refresh);
-  };
+    setPageNum(1)
+    setImageList([])
+    setSelectKey([])
+    setRefresh(!refresh)
+  }
 
   const onChange = (e: any, id: number) => {
-    e.preventDefault();
-    e.stopPropagation();
-    const arr = [...selectKey];
-    if (arr.indexOf(id) === -1) {
-      arr.push(id);
-    } else {
-      arr.splice(arr.indexOf(id), 1);
-    }
-    setSelectKey(arr);
-  };
+    // 阻止默认事件
+    e.preventDefault()
+    e.stopPropagation()
+    const arr = [...selectKey]
+    if (!arr.includes(id))
+      arr.push(id)
+    else
+      arr.splice(arr.indexOf(id), 1)
+
+    setSelectKey(arr)
+  }
 
   const selectAll = () => {
-    let arr = [];
-    for (let i = 0; i < imageList.length; i++) {
-      arr.push(imageList[i].id);
-    }
-    setSelectKey(arr);
-  };
+    const arr = []
+    for (let i = 0; i < imageList.length; i++)
+      arr.push(imageList[i].id)
+
+    setSelectKey(arr)
+  }
 
   const cancelAll = () => {
-    setSelectKey([]);
-  };
+    setSelectKey([])
+  }
 
   const showImage = (index: number, value: boolean) => {
-    const arr = [...visibleArr];
-    arr[index] = value;
-    setVisibleArr(arr);
-  };
+    const arr = [...visibleArr]
+    arr[index] = value
+    setVisibleArr(arr)
+  }
 
   const showHover = (index: number, value: boolean) => {
-    const arr = [...hoverArr];
-    for (let i = 0; i < arr.length; i++) {
-      arr[i] = false;
-    }
-    arr[index] = value;
-    setHoverArr(arr);
-  };
+    const arr = [...hoverArr]
+    for (let i = 0; i < arr.length; i++)
+      arr[i] = false
+
+    arr[index] = value
+    setHoverArr(arr)
+  }
 
   return (
     <>
@@ -175,9 +178,10 @@ const ResourceImagesPage = () => {
                 <UploadImageSub
                   from={0}
                   onUpdate={() => {
-                    resetImageList();
+                    resetImageList()
                   }}
-                ></UploadImageSub>
+                >
+                </UploadImageSub>
                 <PerButton
                   type="danger"
                   text="删除"
@@ -205,69 +209,70 @@ const ResourceImagesPage = () => {
             </Col>
           </div>
         )}
-        <div className={styles["images-box"]}>
-          {!loading &&
-            imageList.map((item: any, index: number) => (
-              <div
-                key={item.id}
-                className={`${styles.imageItem} ref-image-item`}
-                style={{ backgroundImage: `url(${item.url})` }}
-                onClick={() => showImage(index, true)}
-                onMouseOver={() => showHover(index, true)}
-                onMouseOut={() => showHover(index, false)}
-              >
-                {hoverArr[index] && (
-                  <i
-                    className={styles.checkbox}
-                    onClick={(e) => onChange(e, item.id)}
-                  ></i>
-                )}
-                {selectKey.indexOf(item.id) !== -1 && (
-                  <i
-                    className={styles.checked}
-                    onClick={(e) => onChange(e, item.id)}
-                  >
-                    <CheckOutlined />
-                  </i>
-                )}
-                <Image
-                  width={200}
-                  style={{ display: "none" }}
-                  src={item.url}
-                  preview={{
-                    visible: visibleArr[index],
-                    src: item.url,
-                    onVisibleChange: (value) => {
-                      showImage(index, value);
-                    },
-                  }}
-                />
-              </div>
-            ))}
+        <div className={styles['images-box']}>
+          {!loading
+          && imageList.map((item: any, index: number) => (
+            <div
+              key={item.id}
+              className={`${styles.imageItem} ref-image-item`}
+              style={{ backgroundImage: `url(${item.url})` }}
+              onClick={() => showImage(index, true)}
+              onMouseOver={() => showHover(index, true)}
+              onMouseOut={() => showHover(index, false)}
+            >
+              {hoverArr[index] && (
+                <i
+                  className={styles.checkbox}
+                  onClick={e => onChange(e, item.id)}
+                >
+                </i>
+              )}
+              {selectKey.includes(item.id) && (
+                <i
+                  className={styles.checked}
+                  onClick={e => onChange(e, item.id)}
+                >
+                  <CheckOutlined />
+                </i>
+              )}
+              <Image
+                width={200}
+                style={{ display: 'none' }}
+                src={item.url}
+                preview={{
+                  visible: visibleArr[index],
+                  src: item.url,
+                  onVisibleChange: (value) => {
+                    showImage(index, value)
+                  },
+                }}
+              />
+            </div>
+          ))}
         </div>
         {!loading && imageList.length > 0 && (
           <Col
             span={24}
             style={{
-              display: "flex",
-              flexDirection: "row-reverse",
+              display: 'flex',
+              flexDirection: 'row-reverse',
               marginTop: 24,
             }}
           >
             <Pagination
               onChange={(currentPage, currentSize) => {
-                setPage(currentPage);
-                setSize(currentSize);
+                setPageNum(currentPage)
+                setPageSize(currentSize)
               }}
-              defaultCurrent={page}
+              defaultCurrent={pageNum}
               total={total}
-              pageSize={size}
+              pageSize={pageSize}
             />
           </Col>
         )}
       </div>
     </>
-  );
-};
+  )
+}
 
-export default ResourceImagesPage;
+export default ResourceImagesPage
