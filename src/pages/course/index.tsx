cead4c69c439bec3929273dbfbdc77519cb1f9b7
Poison02 <2069820192@qq.com>
2024-02-08 16:53:33 +0800

    更改logo和favicon

--- src/pages/course/index.tsx
+++ src/pages/course/index.tsx
@@ -97,7 +97,7 @@ function CoursePage() {
     course.getCourseCategory({
       type: 'REPLAY_COURSE',
     }).then((res: any) => {
-      const categories = res.data.categories
+      const categories = res.data
       const box: any = []
       for (let i = 0; i < categories.length; i++) {
         // 判断是否有二级分类
