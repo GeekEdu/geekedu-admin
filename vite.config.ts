ab0fdffed75a766af17349084d88c5bbc6bd148b
Poison02 <2069820192@qq.com>
2024-03-16 16:33:21 +0800

    更改vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import gzipPlugin from "rollup-plugin-gzip";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import gzipPlugin from 'rollup-plugin-gzip'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,4 +13,4 @@ export default defineConfig({
       plugins: [gzipPlugin()],
     },
   },
-});
+})
